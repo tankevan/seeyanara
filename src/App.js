f02ddcaa079f8389b4c809309cfc9f85353de686
Kevan Tan <kevan@open.gov.sg>
2021-03-11 00:40:28 +0800

    feat: added footer and tweaked header/footer margins

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Heading, useBreakpointValue } from "@chakra-ui/react";
 
 import "./App.scss";
 
 // Components
 import Grid from "./components/Grid";
+import Footer from './components/Footer';
 
 // Data
 import content from "./content";
 
 function App() {
+  const HeadingMarginBreakpoints = useBreakpointValue({ base: "12", lg: "24" })
+
   return (
     <Container maxW="full" centerContent>
-      <Heading as="h1" size="2xl" m="12">
+      <Heading as="h1" size="2xl" m={HeadingMarginBreakpoints}>
         {content.page_title}
       </Heading>
       <Grid />
+      <Footer />
     </Container>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react'
+
+export default function Footer() {
+  return (
+    <Box pt={24} pb={24}>
+      <Text>With ❤️, from the folks at <a href="https://open.gov.sg">OGP</a></Text>
+    </Box>
+  )
+}
