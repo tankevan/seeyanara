882465d300e177dcb0e7c5bbdd91ae63f9a13045
yixin <angyixin@gmail.com>
2021-03-10 20:38:57 +0800

    try to fix 1 part of the padding

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading } from "@chakra-ui/react";
 
-import './App.scss';
-
+import "./App.scss";
 
 // Components
-import Grid from './components/Grid'
+import Grid from "./components/Grid";
 
 // Data
-import content from './content'
+import content from "./content";
 
 function App() {
   return (
-    <Container centerContent maxW="full">
-      <Heading as="h1" size="2xl" m="12">{content.page_title}</Heading>
+    <Container maxW="full" centerContent>
+      <Heading as="h1" size="2xl" m="12">
+        {content.page_title}
+      </Heading>
       <Grid />
     </Container>
   );
